22fd045d5f7e3963bec4d2f62eb31ed8fa987e4a
ivqteno <gingerwortel.vanoranje@gmail.com>
2025-12-29 03:28:19 +0100

    Create Login page component using LoginScreen

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,45 +1,10 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import LoginScreen from "@/components/LoginScreen";
-import RegisterScreen from "@/components/RegisterScreen";
+import React from 'react';
+import LoginScreen from '../components/LoginScreen';
 
-const Login = () => {
-  const navigate = useNavigate();
-  const [showRegister, setShowRegister] = useState(false);
-
-  const handleLoginSuccess = () => {
-    sessionStorage.setItem("xyfen_authenticated", "true");
-    localStorage.setItem("xyfen_authenticated", "true");
-    window.location.href = "/";
-  };
-
-  const handleRegisterSuccess = () => {
-    sessionStorage.setItem("xyfen_authenticated", "true");
-    localStorage.setItem("xyfen_authenticated", "true");
-    window.location.href = "/";
-  };
-
-  const handleSwitchToRegister = () => {
-    navigate("/register");
-  };
-
-  if (showRegister) {
-    return (
-      <div className="min-h-screen flex items-center justify-center bg-background p-4">
-        <RegisterScreen
-          onRegisterSuccess={handleRegisterSuccess}
-          onSwitchToLogin={() => setShowRegister(false)}
-        />
-      </div>
-    );
-  }
-
+const Login: React.FC = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-background p-4">
-      <LoginScreen
-        onLoginSuccess={handleLoginSuccess}
-        onSwitchToRegister={handleSwitchToRegister}
-      />
+    <div className="login-page">
+      <LoginScreen />
     </div>
   );
 };
