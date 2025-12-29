b55bca69d22b157678cd35dc0f812d6731381376
ivqteno <gingerwortel.vanoranje@gmail.com>
2025-12-29 03:37:13 +0100

    Updated Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
-import LoginScreen from '../components/LoginScreen';
+import { useNavigate } from "react-router-dom";
+import LoginScreen from "@/components/LoginScreen";
 
-const Login: React.FC = () => {
+const Login = () => {
+  const navigate = useNavigate();
+
+  const handleLoginSuccess = () => {
+    sessionStorage.setItem("xyfen_authenticated", "true");
+    localStorage.setItem("xyfen_authenticated", "true");
+    window.location.href = "/";
+  };
+
+  const handleSwitchToRegister = () => {
+    navigate("/register");
+  };
+
   return (
-    <div className="login-page">
-      <LoginScreen />
+    <div className="min-h-screen flex items-center justify-center bg-background p-4">
+      <LoginScreen
+        onLoginSuccess={handleLoginSuccess}
+        onSwitchToRegister={handleSwitchToRegister}
+      />
     </div>
   );
 };
